1ccc6b333dde8f73f607e51c23f0e39e213b07ba
reiyaono <reiya018018s@gmail.com>
2022-09-23 11:45:36 +0900

    develop environment

---
+++ src/themes/fontSizes.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const fontSize: any = [12, 14, 16, 20, 24, 32]
+
+
+// alias
+fontSize.extraSmall = fontSize[0]
+fontSize.small = fontSize[1]
+fontSize.medium = fontSize[2]
+fontSize.mediumLarge = fontSize[3]
+fontSize.large = fontSize[4]
+fontSize.extraLarge = fontSize[5]
+
+export default fontSize
---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+import fontSizes from './fontSizes'
+import letterSpacings from './letterSpacings'
+import lineHeights from './lineHeights'
+import space from './space'
+import colors from './colors'
+
+export const theme = {
+  space,
+  fontSizes,
+  letterSpacings,
+  lineHeights,
+  colors
+} as const
